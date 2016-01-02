9c047f442c8d125f62a29ee8b35c2f3285ed2a4c
argerim <argerim@gmail.com>
2016-01-02 11:08:16 -0200

    populate seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+map = Map.create(name: 'SP')
+
+City.create([
+  { name: 'A', map: map},
+  { name: 'B', map: map },
+  { name: 'C', map: map },
+  { name: 'D', map: map },
+  { name: 'E', map: map }
+])
+
+Route.create([
+  { origin: City.by_name("A"), destination: City.by_name("B"), distance: 10 },
+  { origin: City.by_name("B"), destination: City.by_name("D"), distance: 15 },
+  { origin: City.by_name("A"), destination: City.by_name("C"), distance: 20 },
+  { origin: City.by_name("C"), destination: City.by_name("D"), distance: 30 },
+  { origin: City.by_name("B"), destination: City.by_name("E"), distance: 50 },
+  { origin: City.by_name("D"), destination: City.by_name("E"), distance: 30 }
+])
