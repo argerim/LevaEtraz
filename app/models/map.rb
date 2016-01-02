d0efc039595a32a9401f3c44c4bb5464f1f95fc3
argerim <argerim@gmail.com>
2016-01-02 04:00:46 -0200

    dependet destroy on map.

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,6 +1,6 @@
 class Map < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-  has_many :cities
+  has_many :cities, dependent: :destroy
 
   scope :by_name, -> (name) { where(name: name).first_or_create }
 end
