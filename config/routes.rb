be6f657d8d33cf6149969687b9773561dc2f0b84
argerim <argerim@gmail.com>
2016-01-02 00:07:47 -0200

    create routes cities controller.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :maps, only: [:update, :create, :index, :destroy]
+      resources :cities, only: [:update, :create, :index, :destroy]
     end
   end
 end
