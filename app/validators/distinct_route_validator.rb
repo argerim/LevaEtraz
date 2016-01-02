6a60af006784c20a46e6327abca913c5ef1b6e86
argerim <argerim@gmail.com>
2016-01-02 03:32:17 -0200

    add attribute to validator custom.

--- app/validators/distinct_route_validator.rb
+++ app/validators/distinct_route_validator.rb
@@ -2,7 +2,7 @@ class DistinctRouteValidator < ActiveModel::Validator
   
   def validate(record)
     if record.origin.eql?(record.destination)
-      record.errors.add :base, 'must be different from the target'
+      record.errors.add :origin_id, 'must be different from the target'
     end
   end
 
