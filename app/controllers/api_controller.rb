470acd7cf225337867ca8f205502a66e67887648
argerim <argerim@gmail.com>
2016-01-02 01:53:53 -0200

    call concern and add new route.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,4 +2,5 @@ class ApiController < ActionController::Base
 
   protect_from_forgery with: :null_session
   
+  include RescueFrom
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :maps, only: [:update, :create, :index, :destroy]
       resources :cities, only: [:update, :create, :index, :destroy]
+      resources :routes, only: [:create]
     end
   end
 end
